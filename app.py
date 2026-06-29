9a3f73b7b401a8bb0769a2ee3e09d4646dd58d4f
tirathram2 <ramt40396@gmail.com>
2026-06-29 21:44:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_socketio import SocketIO, send
 import os
+import sqlite3
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret"
